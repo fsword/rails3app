8507b7f2e97e21d6e05a40c806d4643e49e5f55b
fsword <li.jianye@gmail.com>
2010-09-19 00:51:37 +0800

    Generate a new rails3 app for oauth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oauthapp::Application.config.secret_token = 'd8c54842b6634d1a4ee3aff53ae9ce2067116eb450e7f8b04e052bc8997a947eec509383bf65e73dbdf812ebe41897f5baf4ca909e4a9cc48f2af55c9280945d'
