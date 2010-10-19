b132aded3bf3ae2e9f62cd743d3f180c124ac22b
fsword <li.jianye@gmail.com>
2010-10-19 17:00:11 +0800

    support sina + douban oauth

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,9 @@ class Profile < ActiveRecord::Base
   belongs_to :user
 
   def after_update
+    if self.user.sina?
+      user.sina.post "http://api.t.sina.com.cn/statuses/update.xml", {:status => self_desc}
+    end
     if self.user.douban?
       user.douban.post "http://api.douban.com/miniblog/saying", %Q{<?xml version='1.0' encoding='UTF-8'?><entry xmlns:ns0="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/"><content>#{self_desc}</content></entry>},  {"Content-Type" =>  "application/atom+xml"}
     end
